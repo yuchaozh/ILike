9f93afaa02aedea950bcada60008fa59523eadda
Yuchao Zhou <yzhou@enspirea.com>
2015-12-28 14:44:39 -0600

    first commit

---
+++ ILike/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ILike
+//
+//  Created by Enspirea LLC on 15/12/28.
+//  Copyright © 2015年 yuchaozh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ILike/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ILike
+//
+//  Created by Enspirea LLC on 15/12/28.
+//  Copyright © 2015年 yuchaozh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
